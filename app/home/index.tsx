0f93ba3ff79f3ea65dccf9bbe7a302fa1fd1882e
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-20 20:28:05 +0900

    app/home/index.tsxを作成

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -9,19 +9,21 @@ export default function HomeScreen() {
 
   const handleAddLabelPress = () => {};
 
+  const handleEditLabelPress = () => {};
+
   return (
     <View style={styles.container}>
-      <Button title="ラベル追加" onPress={} />
+      <Button title="ラベル追加" onPress={handleAddLabelPress} />
 
-      <Button title="全てのメモ" onPress={} />
+      <Button title="全てのメモ" onPress={handleAllMemoPress} />
 
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Button title="ラベル1" onPress={} />
+        <Button title="ラベル1" onPress={handleEditLabelPress} />
         <MaterialIcons name="edit" size={24} color={'gray'} onPress={} />
       </View>
 
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Button title="ラベル2" onPress={} />
+        <Button title="ラベル2" onPress={handleLabelPress} />
         <MaterialIcons name="edit" size={24} color={'gray'} onPress={} />
       </View>
     </View>
