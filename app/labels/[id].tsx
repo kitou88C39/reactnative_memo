34e4a9bee2de74742530b4038271cce5cdc318f8
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-21 16:04:46 +0900

    app/labels/[id].tsxを作成

---
+++ app/labels/[id].tsx
@@ -0,0 +1,27 @@
+//ラベル画面作成
+import { router } from 'expo-router';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function LabelCreateScreen() {
+  const handleCreatePress = () => {
+    router.dismiss();
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tittle}>ラベル作成</Text>
+      <Button title="作成" onPress={handleCreatePress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EFEFF4',
+    justifyContent: 'center'
+  },
+  tittle: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
