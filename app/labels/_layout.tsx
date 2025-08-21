19cb09b7a6a9a214b61c23bddbecee89db574c67
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-21 16:40:38 +0900

    app/labels/_layout.tsxを作成

--- app/labels/_layout.tsx
+++ app/labels/_layout.tsx
@@ -1,17 +1,5 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return (
-    <Stack screenOptions={{ headerTintColor: '#0000000', headerStyle: { backgroundColor: '#F9F9F9' } }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      {/*ホーム*/}
-      <Stack.Screen name="home/index" options={{ headerTitle: 'ホーム' }} />
-      {/*ラベル*/}
-      <Stack.Screen name="labels" options={{ headerShown: false, presentation: 'fullScreenModal' }} />
-      {/*メモ*/}
-      <Stack.Screen name="memos/index" options={{ headerTitle: 'メモ' }} />
-      <Stack.Screen name="memos/create" options={{ headerTitle: '' }} />
-      <Stack.Screen name="memos/[id]" options={{ headerTitle: '' }} />
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerTintColor: '#0000000', headerStyle: { backgroundColor: '#F9F9F9' } }}></Stack>;
 }
