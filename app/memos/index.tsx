19947b964f7871378daedaa84f31ed1004d948d2
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-21 19:36:49 +0900

    app/memos/index.tsxを作成

---
+++ app/memos/index.tsx
@@ -0,0 +1,29 @@
+//メモ修正画面
+import { router, useLocalSearchParams } from 'expo-router';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function MemoEditScreen() {
+  const { id } = useLocalSearchParams;
+
+  const handleSavePress = () => {
+    router.back();
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tittle}>メモ修正: {id}</Text>
+      <Button title="保存" onPress={handleSavePress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EFEFF4',
+    justifyContent: 'center'
+  },
+  tittle: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
