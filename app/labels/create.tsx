e72f461b8a3fb2126de70f00710352bb51043f18
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-21 15:53:14 +0900

    app/labels/create.tsxを作成

---
+++ app/labels/create.tsx
@@ -0,0 +1,30 @@
+//ラベル画面作成
+import { router, Router } from 'expo-router';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function InitialScreen() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace('/home');
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tittle}>アプリ起動中・・</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EFEFF4',
+    justifyContent: 'center'
+  },
+  tittle: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
