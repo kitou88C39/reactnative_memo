751ea54d363850252f3d4f89801917dc45638d91
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-21 16:54:03 +0900

    app/labels/_layout.tsxを作成

--- app/labels/_layout.tsx
+++ app/labels/_layout.tsx
@@ -1,12 +1,21 @@
 import { Stack } from 'expo-router';
 import { Text, TouchableOpacity } from 'react-native';
+import { router } from 'expo-router';
 
 export default function Layout() {
   return (
     <Stack screenOptions={{ headerShown: true }}>
-      <Stack.Screen name="create" options={{ headerTitle: 'ラベル作成', headerLeft: () =>
-        <TouchableOpacity>
-          <Text></Text>
-      </TouchableOpacity>
-  )}}
-</Stack>
+      <Stack.Screen
+        name="create"
+        options={{
+          headerTitle: 'ラベル作成',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.dismiss()}>
+              <Text>閉じる</Text>
+            </TouchableOpacity>
+          )
+        }}
+      />
+    </Stack>
+  );
+}
