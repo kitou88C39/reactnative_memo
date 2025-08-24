a0df6e5e8212762e11522494890df2fa85668923
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-24 19:43:40 +0900

    app/_layout.tsx にプロバイダーを追加する

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,21 @@
 import { Stack } from 'expo-router';
+import { config } from '@gluestack-ui/config';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerTintColor: '#0000000', headerStyle: { backgroundColor: '#F9F9F9' } }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      {/*ホーム*/}
-      <Stack.Screen name="home/index" options={{ headerTitle: 'ホーム' }} />
-      {/*ラベル*/}
-      <Stack.Screen name="labels" options={{ headerShown: false, presentation: 'fullScreenModal' }} />
-      {/*メモ*/}
-      <Stack.Screen name="memos/index" options={{ headerTitle: 'メモ' }} />
-      <Stack.Screen name="memos/create" options={{ headerTitle: '' }} />
-      <Stack.Screen name="memos/[id]" options={{ headerTitle: '' }} />
-    </Stack>
+    <GluestackUIProvider config={config}>
+      <Stack screenOptions={{ headerTintColor: '#0000000', headerStyle: { backgroundColor: '#F9F9F9' } }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/*ホーム*/}
+        <Stack.Screen name="home/index" options={{ headerTitle: 'ホーム' }} />
+        {/*ラベル*/}
+        <Stack.Screen name="labels" options={{ headerShown: false, presentation: 'fullScreenModal' }} />
+        {/*メモ*/}
+        <Stack.Screen name="memos/index" options={{ headerTitle: 'メモ' }} />
+        <Stack.Screen name="memos/create" options={{ headerTitle: '' }} />
+        <Stack.Screen name="memos/[id]" options={{ headerTitle: '' }} />
+      </Stack>
+    </GluestackUIProvider>
   );
 }
