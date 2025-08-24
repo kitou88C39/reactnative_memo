cbb4c42a082d4c4d17a64885ed05250af2740fb1
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-24 20:31:27 +0900

    src/components/MemoInputForm.tsxを作成する

--- src/components/MemoInputForm.tsx
+++ src/components/MemoInputForm.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Input, InputField, Textarea, TextareaInput } from '@gluestack-ui/themed';
 
 type MemoInputFormProps = {
   title: string;
@@ -9,4 +10,10 @@ type MemoInputFormProps = {
 
 const MemoInputForm: React.FC<MemoInputFormProps> = props => {
   const { content, title, onTitleChange, onContentChange } = props;
+
+  return (
+    <View style={{ flex: 1, paddingBottom: 100 }}>
+      <Textarea borderWidth={0} minWidth={'$full'} minHeight={'$full'}></Textarea>
+    </View>
+  );
 };
