6c68198512b29799fea52e286f7c212be7eaae0a
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-24 17:05:22 +0900

    src/components/LabelTag.tsxを作成

--- src/components/LabelTag.tsx
+++ src/components/LabelTag.tsx
@@ -1,9 +1,14 @@
+import { Text, View } from 'react-native';
+
 type LabelTagProps = {
   color: string;
   name: string;
-  onPress: () => void;
-  onEditPress: () => void;
 };
 
 const LabelTag: React.FC<LabelTagProps> = props => {
-  const { color, name, onPress, onEditPress } = props;
+  const { color, name } = props;
+
+  return () => {};
+};
+
+export { LabelTag };
