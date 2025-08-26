841333c2e9bca4f3655b13afe9a279ed9e02f51d
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-26 19:48:13 +0900

    src/types/memo.tsを作成

--- src/types/memo.ts
+++ src/types/memo.ts
@@ -1,5 +1,7 @@
-const MEMO_DATA = [
-  { id: 'ABCD', name: 'useStateについて', content: 'blue', label: { color: 'blue', name: 'プログラミング' } },
-  { id: 'EFGH', name: 'アカウント', content: 'メールアドレス' },
-  { id: 'IJKL', name: 'オムライス', content: '卵' }
-];
+type Memo = {
+  id: string;
+  name: string;
+  content: string;
+  labelId: number | undefined;
+};
+export type { Memo };
