49c5407d72ff906226c637f8702e4ed3bdd2a89b
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-26 17:29:27 +0900

    src/dummy_data/labelData.tsを作成

--- src/dummy_data/labelData.ts
+++ src/dummy_data/labelData.ts
@@ -1,5 +1,8 @@
-const LABEL_DATA = [
+import { type Label } from '../types/label';
+
+const LABEL_DATA: Label[] = [
   { id: 1, name: 'プログラミング', color: 'blue' },
   { id: 2, name: 'パスワード', color: 'green' },
   { id: 3, name: '料理', color: 'orange' }
 ];
+export { LABEL_DATA };
