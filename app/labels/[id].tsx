dbf87005b72ceb22590e54df5b8d5c318076fc8b
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-26 13:42:43 +0900

    app/labels/[id].tsxを修正

--- app/labels/[id].tsx
+++ app/labels/[id].tsx
@@ -1,23 +1,32 @@
 //ラベル修正画面
+import { Input, InputField, VStack } from '@gluestack-ui/themed';
 import { router, useLocalSearchParams } from 'expo-router';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { Input, InputField } from '@gluestack-ui/themed';
 import { useState } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import { ColorPicker } from '../../src/components/ColorPicker';
 
 export default function LabelEditScreen() {
   const { id } = useLocalSearchParams();
 
   const [labelName, setLabelName] = useState<String>('');
+  const [color, setColor] = useState<String | undefined>(undefined);
+
+  const handleColorPress = (color: string) => {
+    setColor(color);
+  };
 
   const handleEditPress = () => {
     router.dismiss();
   };
   return (
     <View style={styles.container}>
-      <Input variant="underlined" size="md" backgroundColor="$white" borderBlockColor="$warmGray400">
-        <InputField padding={'$2'} placeholder="ラベル名" onChangeText={setLabelName} />
-      </Input>
-      <Button title="修正" onPress={handleEditPress} />
+      <VStack space="lg">
+        <Input variant="underlined" size="md" backgroundColor="$white" borderBlockColor="$warmGray400">
+          <InputField padding={'$2'} placeholder="ラベル名" onChangeText={setLabelName} />
+        </Input>
+        <ColorPicker onPress={handleColorPress} />
+        <Button title="修正" onPress={handleEditPress} />
+      </VStack>
     </View>
   );
 }
