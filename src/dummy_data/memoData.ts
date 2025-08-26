0230ac295cf6022679bf31a2d0df51a4eb0a0936
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-26 19:22:09 +0900

    src/dummy_data/memoData.tsを作成

---
+++ src/dummy_data/memoData.ts
@@ -0,0 +1,5 @@
+const MEMO_DATA = [
+  { id: 'ABCD', name: 'useStateについて', content: 'blue', label: { color: 'blue', name: 'プログラミング' } },
+  { id: 'EFGH', name: 'アカウント', content: 'メールアドレス' },
+  { id: 'IJKL', name: 'オムライス', content: '卵' }
+];
