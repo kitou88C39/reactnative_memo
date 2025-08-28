8bd3b9c7a6f29094f605f62c20e38a825e1d0543
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-28 19:57:34 +0900

    src/database/queries/memoQueries.ts を作成

--- src/database/queries/labelQueries.ts
+++ src/database/queries/labelQueries.ts
@@ -1,11 +1,12 @@
 const CreateTableLabels = `
-CREATE TABLE IF NOT EXISTS labels(
+CREATE TABLE IF NOT EXISTS labels
+  (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     color TEXT NOT NULL,
     created_at TEXT DEFAULT(DATETIME('now','localtime')),
     updated_at TEXT DEFAULT(DATETIME('now','localtime'))
-)
+  );
 `;
 
 const LabelQueries = Object.freeze({
