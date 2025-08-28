3822721c52eae3b3e8b946be0148532a32d91dcc
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-28 20:49:50 +0900

    src/database/services/labelServices.tsを作成

--- src/services/labelServices.ts
+++ src/services/labelServices.ts
@@ -1,4 +1,8 @@
-import { execute } from '../database/dbService';
+import { excute as execute } from '../database/dbService';
 import { LabelQueries } from '../database/queries/labelQueries';
 
-const createTable = async () => {};
+const createTable = async () => {
+  await execute({ sql: LabelQueries.CREATE_TABLE });
+};
+
+export { createTable };
