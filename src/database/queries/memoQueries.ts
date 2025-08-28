170567f153c2779e2f7395cbcd34d0d5f2c734da
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-28 19:54:36 +0900

    src/database/queries/memoQueries.ts を作成

--- src/database/queries/memoQueries.ts
+++ src/database/queries/memoQueries.ts
@@ -1,15 +1,19 @@
-const CreateTableLabels = `
-CREATE TABLE IF NOT EXISTS labels(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    color TEXT NOT NULL,
+const CreateTableMemos = `
+CREATE TABLE IF NOT EXISTS memos
+(
+    id TEXT,
+    labelId  INTEGER,
+    title TEXT NOT NULL,
+    content TEXT,
     created_at TEXT DEFAULT(DATETIME('now','localtime')),
     updated_at TEXT DEFAULT(DATETIME('now','localtime'))
+    PRIMARY KEY(id),
+    FOREIGN KEY(labelId) REFERENCES labels(id),
 )
 `;
 
-const LabelQueries = Object.freeze({
-  CREATE_TABLE: CreateTableLabels
+const MemolQueries = Object.freeze({
+  CREATE_TABLE: CreateTableMemos
 });
 
-export { LabelQueries };
+export { MemolQueries };
