ed8eb624431e0872d4c6baf7104fb470302c750f
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-29 09:55:37 +0900

    src/database/queries/memoQueries.tsを修正

--- src/services/memoServices.ts
+++ src/services/memoServices.ts
@@ -1,8 +1,14 @@
 import { excute as execute } from '../database/dbService';
 import { MemolQueries } from '../database/queries/memoQueries';
+import * as Crypto from 'expo-crypto';
 
 const createTable = async () => {
   await execute({ sql: MemolQueries.CREATE_TABLE });
+};
+
+const addMemo = async (title: string, content: string) => {
+  const memoId = Crypto.randomUUID;
+  await execute({ sql: MemolQueries.INSERT, params: [memoId, title, content] });
 };
 
 export { createTable };
