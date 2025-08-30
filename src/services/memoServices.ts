4e91ec6d83df5c6f526f66641c8d071687989d0b
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-30 15:10:02 +0900

    src/database/services/memoServices.tsを修正

--- src/services/memoServices.ts
+++ src/services/memoServices.ts
@@ -21,6 +21,19 @@ const getMemos = async (): Promise<Memo[]> => {
   return memos;
 };
 
+const getMemos = async (): Promise<Memo[]> => {
+  const rows = await fetch<MemoSchema>({ sql: MemoQueries.SELECT });
+  const memos = rows.map((row): Memo => {
+    return {
+      id: row.id,
+      title: row.title,
+      content: row.content || '',
+      labelId: row.label_id || undefined
+    };
+  });
+  return memos;
+};
+
 const addMemo = async (title: string, content: string) => {
   const memoId = Crypto.randomUUID();
   await execute({ sql: MemoQueries.INSERT, params: [memoId, title, content] });
