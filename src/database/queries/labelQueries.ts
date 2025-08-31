6b83548a2d74f61852f4b28440ed0daa8b508f27
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-31 11:06:22 +0900

    src/database/queries/labelQueries.tsを修正

--- src/database/queries/labelQueries.ts
+++ src/database/queries/labelQueries.ts
@@ -9,6 +9,17 @@ CREATE TABLE IF NOT EXISTS labels
   );
 `;
 
+const CreateTableLabels = `
+CREATE TABLE IF NOT EXISTS labels
+  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    color TEXT NOT NULL,
+    created_at TEXT DEFAULT(DATETIME('now','localtime')),
+    updated_at TEXT DEFAULT(DATETIME('now','localtime'))
+  );
+`;
+
 const InsertLabel = `
 INSERT INTO labels(
   name,
