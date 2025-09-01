70bbdc4430425340582dc5c86cecebe28137aca3
kitoumasaomi <kanfutrooper@gmail.com>
2025-09-01 10:37:22 +0900

    src/dummy_data/labelData.tsを削除

--- src/dummy_data/labelData.ts
+++
@@ -1,8 +0,0 @@
-import { type Label } from '../types/label';
-
-const LABEL_DATA: Label[] = [
-  { id: 1, name: 'プログラミング', color: 'blue' },
-  { id: 2, name: 'パスワード', color: 'green' },
-  { id: 3, name: '料理', color: 'orange' }
-];
-export { LABEL_DATA };
